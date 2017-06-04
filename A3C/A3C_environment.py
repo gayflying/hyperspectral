7b9bf42ae2acfe84cbc917f9354347ad3b3eab51
cjl <cuijialiang@pku.edu.cn>
2017-06-04 12:08:23 +0800

    add file A3C_environment.py

---
+++ A3C/A3C_environment.py
@@ -0,0 +1,18 @@
+#environment class for finantial RL by @jialiang.cui
+
+class env:
+    def __init(self):
+        action_space = []
+        observation_space = []
+
+    def reset(self):
+
+    def step(self, action):
+        observation = []
+        reward = []
+        done = true
+        info = []
+        return [observation, reward, done, info]
+
+    
+
