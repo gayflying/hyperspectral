a8dba7a82b3d20f137a61881dd6bd296e1d15e38
wolffytom <cuijialiang@pku.edu.cn>
2017-06-04 22:00:22 +0800

    modified environment

--- A3C/A3C_environment.py
+++ A3C/A3C_environment.py
@@ -1,18 +1,29 @@
 #environment class for finantial RL by @jialiang.cui
+import gym
+import numpy as np
 
-class env:
-    def __init(self):
-        action_space = []
-        observation_space = []
+class futuresGame:
+    def __init(self,futuresNum = 3,inforFieldsNum = 2):
+        self.mFuturesNum = futuresNum
+        self.inforFieldsNum = inforFieldsNum
+        self.ction_space = gym.spaces.box.Box(np.linspace(-1,-1,futuresNum+1),np.linspace(1,1,futuresNum+1))
+        self.observation_space = gym.spaces.box.Box(np.zeros(inforFieldsNum * futuresNum),np.linspace(100000000,100000000,inforFieldsNum * futuresNum))
+        
 
     def reset(self):
+        self.mProperty = 10000
+        self.mAssetAllocation = np.zeros(self.mFuturesNum + 1)
+        self.mAssetAllocation[-1] = 1
+        return self.observation_space.sample()
+
 
     def step(self, action):
-        observation = []
-        reward = []
-        done = true
-        info = []
-        return [observation, reward, done, info]
+        #update property and assert allocation
 
-    
-
+        #update observation
+        observation = self.observation_space.sample()
+        #update assert allocation num
+        reward = 100
+        done = false
+        info = {}
+        return [observation, assetAllocation, reward, done, info]
